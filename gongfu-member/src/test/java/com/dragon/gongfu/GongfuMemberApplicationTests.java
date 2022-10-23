cce49f117e3a5aa829ec205a27ea775f6392cbd0
ruansl <13143123730@163.com>
2022-10-24 00:05:31 +0800

    add：初始化项目架构

---
+++ gongfu-member/src/test/java/com/dragon/gongfu/GongfuMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.gongfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongfuMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gongfu-product/src/test/java/com/dragon/gongfu/GongfuProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dragon.gongfu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GongfuProductApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
